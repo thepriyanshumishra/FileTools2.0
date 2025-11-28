ad21dff92e644cec168235ebbf2ba157d561c8b5
Priyanshu Mishra <thedarkpcm@gmail.com>
2025-11-28 23:51:12 +0530

    Remove Discord URL and update application title to FileTools

--- src/lib/sections/about/index.ts
+++ src/lib/sections/about/index.ts
@@ -2,4 +2,4 @@ export { default as Credits } from "./Credits.svelte";
 export { default as Donate } from "./Donate.svelte";
 export { default as Resources } from "./Resources.svelte";
 export { default as Why } from "./Why.svelte";
-export { default as Sponsors } from "./Sponsors.svelte";
+
--- src/lib/util/consts.ts
+++ src/lib/util/consts.ts
@@ -3,13 +3,7 @@ import { PUB_DISABLE_ALL_EXTERNAL_REQUESTS, PUB_ENV } from "$env/static/public";
 export const GITHUB_URL_FILETOOLS = "https://github.com/thepriyanshumishra/FileTools2.0";
 export const GITHUB_URL_FILETOOLSD = "https://github.com/thepriyanshumishra/FileTools2.0"; // Assuming same repo for now
 export const GITHUB_API_URL = "https://api.github.com/repos/thepriyanshumishra/FileTools2.0";
-export const DISCORD_URL = "https://discord.gg/kqevGxYPak";
-export const FILETOOLS_NAME =
-	PUB_ENV === "development"
-		? "FileTools Local"
-		: PUB_ENV === "nightly"
-			? "FileTools Nightly"
-			: "FileTools.app";
+export const FILETOOLS_NAME = "FileTools";
 export const CONTACT_EMAIL = "hello@filetools.app";
 
 // i'm not entirely sure this should be in consts.ts, but it is technically a constant as .env is static for FileTools
